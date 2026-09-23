9cf552dc7acdabc3cfbcf1feb8efb5f7c3ef91c8
Jianqiang <jianqiang@example.com>
2026-09-23 09:42:56 +0800

    Remove temp files

--- ss.ps1
+++
@@ -1,7 +0,0 @@
-Add-Type -AssemblyName System.Windows.Forms
-Add-Type -AssemblyName System.Drawing
- = New-Object System.Drawing.Bitmap([System.Windows.Forms.Screen]::PrimaryScreen.Bounds.Width, [System.Windows.Forms.Screen]::PrimaryScreen.Bounds.Height)
- = [System.Drawing.Graphics]::FromImage()
-.CopyFromScreen(0, 0, 0, 0, .Size)
-.Save('D:\wjq-obsidian\personal-site\screen.png', [System.Drawing.Imaging.ImageFormat]::Png)
-Write-Output done
